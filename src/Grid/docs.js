598314fee0d2f883dc94d85a82d121db62c85672
Sam Margalit <sam@margalit.com.au>
2016-08-16 17:31:55 +1000

    Add padding options to grid.

--- src/Grid/docs.js
+++ src/Grid/docs.js
@@ -1,21 +1,50 @@
 /* @flow */
 
 import React from 'react'
-import ItemString from '!raw!..//Item'
+import GridString from '!raw!..//Grid'
 import { parse, } from 'react-docgen'
-import { Grid, Avatar } from '../'
+import { Grid, Avatar, Item, Row, Text, Action } from '../'
+
+const data = {
+  name: 'Mia Mollie', diagnosis: 'Ear Infection', documents: [1,2,3],
+  date: 1471306921302, link: 'http://google.com',
+  avatar: 'http://placekitten.com/250/250',
+}
+
+const { avatar, name, link, diagnosis, documents, date } = data
 
 const example: React.Element<any> = (
-  <div>
-    <Grid cols={3} align="middle">
-      <Avatar url='http://placekitten.com/240/240' fill />
-    </Grid>
-    <Grid cols={9} align="middle">
-      <Grid cols={9} textAlign="right">Sam Margalit</Grid>
-      <Grid cols={3}>View something</Grid>
-    </Grid>
-  </div>
+  <Item border={true}>
+    <Row>
+      <Grid cols={2} align='middle'>
+        <Avatar url={avatar} fill />
+      </Grid>
+      <Grid cols={10} align='middle' padding={{left: 'medium'}}>
+        <Row>
+          <Grid cols={9} align='middle'>
+            <Text small bold>{name}</Text>
+          </Grid>
+          <Grid cols={3} align='middle' textAlign='right'>
+            <Action icon='right' to={link}>
+              View
+            </Action>
+          </Grid>
+        </Row>
+        <Row>
+          <Text small>{diagnosis}</Text>
+        </Row>
+        <Row>
+          <Grid cols={7} align='middle'>
+            <Text small blue>{documents.length} Document{documents.length !== 1 && 's'}</Text>
+          </Grid>
+          <Grid cols={5} align='middle' textAlign='right'>
+            <Text small>{date}</Text>
+          </Grid>
+        </Row>
+      </Grid>
+    </Row>
+  </Item>
 )
 
-const documentation: Object = parse(ItemString)
-export default { example, documentation, }
+const documentation: Object = parse(GridString)
+export default { example, documentation }
--- src/Grid/index.js
+++ src/Grid/index.js
@@ -2,23 +2,35 @@
 
 import React from 'react'
 import styles from './index.css'
+import paddingStyles from 'traits/padding.css'
 
-type PropsType = {
-  children?:React.Element<any>,
-  cols: 1|2|3|4|5|6|7|8|9|10|11|12,
-  align?: 'top'|'middle'|'bottom'|'baseline',
-  textAlign?: 'right'|'left'|'center'
+type PaddingType = 'small'|'medium'|'large'
+
+type PaddingObjectType = {
+  left?: PaddingType,
+  right?: PaddingType,
+  top?: PaddingType,
+  bottom?: PaddingType,
 }
 
-/** Grid component for breaking up lists */
+type PropsType = {
+  children?: React.Element<any>,
+  cols?: 1|2|3|4|5|6|7|8|9|10|11|12,
+  align?: 'top'|'middle'|'bottom'|'baseline',
+  textAlign?: 'right'|'left'|'center',
+  padding?: PaddingObjectType,
+}
+
+/** 12 column grid component for breaking up Rows */
 
 const Grid = (props: PropsType): React.Element<any> => {
   const style = {
-    width: `${props.cols / 12 * 100}%`
+    width: props.cols ? `${props.cols / 12 * 100}%` : false
   }
   const align = props.align ? capitaliseFirstLetter(props.align) : 'Normal'
   const textAlign = props.textAlign ? capitaliseFirstLetter(props.textAlign) : 'Normal'
-  const className = styles[`Grid${align}${textAlign}`]
+  const paddingClasses = props.padding ? getPaddingClasses(props.padding) : ''
+  const className = `${styles[`Grid${align}${textAlign}`]} ${paddingClasses}`
   return (
     <div className={className} style={style}>
       {props.children}
@@ -26,8 +38,28 @@ const Grid = (props: PropsType): React.Element<any> => {
   )
 }
 
-function capitaliseFirstLetter(string) {
+function capitaliseFirstLetter(string: string): string {
   return string.charAt(0).toUpperCase() + string.slice(1)
+}
+
+function getPaddingClasses(padding: PaddingObjectType): string {
+  return Object.keys(padding).reduce((classes: string, key: string) => {
+    const paddingClass = paddingStyles[`p${key.substring(0,1)}${getPaddingWidth(padding[key])}`]
+    return `${classes} ${paddingClass}`
+  }, '')
+}
+
+function getPaddingWidth(padding: PaddingType): number {
+  switch (padding) {
+  case 'small':
+    return 1
+  case 'medium':
+    return 2
+  case 'large':
+    return 3
+  default:
+    return 2
+  }
 }
 
 export default Grid
