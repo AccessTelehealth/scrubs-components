2c33d73f4f88105ce718686c3dff1950e0cd4a12
Mia <miamollie@gmail.com>
2016-08-17 15:45:31 +1000

    sam is no fun

--- src/Action/docs.js
+++ src/Action/docs.js
@@ -1,22 +1,12 @@
 /* @flow */
 
 import React from 'react'
-import { Action, Row, Grid, Text } from '../'
+import { Action } from '../'
 import ActionString from '!raw!..//Action'
 import { parse } from 'react-docgen'
 
 const example: React.Element<any> = (
-  <Row>
-    <Grid cols={1} align='middle' >
-        <Action icon="arrowLeft" iconSide="left">Prev</Action>
-    </Grid>
-    <Grid textAlign='center' align='middle' cols={10}>
-        <Text>I <Action icon="heart" /> Icons</Text>
-    </Grid>
-    <Grid cols={1} align='middle'>
-        <Action icon="arrowRight" iconSide="right">Next</Action>
-    </Grid>
-  </Row>
+  <Action icon="arrowLeft" iconSide="left">Prev</Action>
 )
 
 const documentation: Object = parse(ActionString)
