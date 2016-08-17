82e5f472b5fa9532df48d8a158041e7e02142ac9
Mia <miamollie@gmail.com>
2016-08-17 15:42:11 +1000

    Add further icon examples to Action component docs

--- src/Action/docs.js
+++ src/Action/docs.js
@@ -1,16 +1,23 @@
 /* @flow */
 
 import React from 'react'
-import { Action, Row } from '../'
+import { Action, Row, Grid, Text } from '../'
 import ActionString from '!raw!..//Action'
 import { parse } from 'react-docgen'
 
 const example: React.Element<any> = (
   <Row>
-    <Action icon="arrowLeft" iconSide="left">Prev</Action>
-    <Action icon="arrowRight" iconSide="right">Next</Action>
+    <Grid cols={1} align='middle' >
+        <Action icon="arrowLeft" iconSide="left">Prev</Action>
+    </Grid>
+    <Grid textAlign='center' align='middle' cols={10}>
+        <Text>I <Action icon="heart" /> Icons</Text>
+    </Grid>
+    <Grid cols={1} align='middle'>
+        <Action icon="arrowRight" iconSide="right">Next</Action>
+    </Grid>
   </Row>
 )
 
 const documentation: Object = parse(ActionString)
-export default { example, documentation, }
+export default { example, documentation }
