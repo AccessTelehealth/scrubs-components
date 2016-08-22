ab58ad09b067d149a5bf5f82ecd32b9a2fc20cc1
Mia <miamollie@gmail.com>
2016-08-22 11:16:15 +1000

    Add Icon Component Docs

--- src/Icon/docs.js
+++ src/Icon/docs.js
@@ -3,11 +3,30 @@
 import React from 'react'
 import IconString from '!raw!..//Icon'
 import { parse } from 'react-docgen'
-import { Icon } from '../'
+import { Icon, Grid, Row } from '../'
 
 const Example = (): React.Element<*> => {
   return (
-    <Icon icon="arrowLeft" size="small" fill="red" />
+    <Row>
+      <Grid textAlign='center' align='middle' cols={2}>
+        <Icon icon="heart" size="large" fill="red" />
+      </Grid>
+      <Grid textAlign='center' align='middle' cols={2}>
+        <Icon icon="phone" size="large" fill="greyDark" />
+      </Grid>
+      <Grid textAlign='center' align='middle' cols={2}>
+        <Icon icon="chat" size="large" />
+      </Grid>
+      <Grid textAlign='center' align='middle' cols={2}>
+        <Icon icon="flipCamera" size="large" fill="green" />
+      </Grid>
+      <Grid textAlign='center' align='middle' cols={2}>
+        <Icon icon="caduceus" size="large" fill="blue" />
+      </Grid>
+      <Grid textAlign='center' align='middle' cols={2}>
+        <Icon icon="home" size="large" fill="grey" />
+      </Grid>
+    </Row>
   )
 }
 
