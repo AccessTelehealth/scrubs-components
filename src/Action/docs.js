bd3d7bbd590dddd537e437b417263514e4c6dff7
Mia <miamollie@gmail.com>
2016-08-22 11:49:26 +1000

    Update Action Component

--- src/Action/docs.js
+++ src/Action/docs.js
@@ -1,12 +1,18 @@
 /* @flow */
 
 import React from 'react'
-import { Action } from '../'
+import { Action, Row, Grid } from '../'
 import ActionString from '!raw!..//Action'
 import { parse } from 'react-docgen'
 
 const Example = (): React.Element<*> => {
-  return <Action icon="arrowLeft" iconSide="left">Prev</Action>
+  return (
+    <Row>
+      <Grid cols={4} align="middle" textAlign='left'><Action icon="arrowLeft" iconSide="left">Prev</Action></Grid>
+      <Grid cols={4} align="middle" textAlign='center'><Action icon="home" iconSide="left"></Action></Grid>
+      <Grid cols={4} align="middle" textAlign='right'><Action icon="arrowRight" iconSide="right">Next</Action></Grid>
+    </Row>
+  )
 }
 
 const documentation: Object = parse(ActionString)
