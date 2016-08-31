2706ff2d161c93787af43616cee98b7d60c93311
Mia <miamollie@gmail.com>
2016-08-31 14:34:05 +1000

    Update style props type annotation

--- components/Input.js
+++ components/Input.js
@@ -5,7 +5,7 @@ const Input = (props) => {
   return (
     <Base
       tagName='input' textBlueDark borderGrey border={1}
-      padding={2} sans rounded fontSize={3} fillWidth
+      padding={2} sans rounded fontSize={5} fillWidth
       antialiased lineHeight={3} bgGrey {...props}
     />
   )
